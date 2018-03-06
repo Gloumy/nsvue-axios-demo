586502cb09b8443a4e440038a997895954b82424
Simon Cornet <simon@creative-atoms.be>
2018-03-06 08:20:23 +0100

    vue init #0.2.0 && npm install

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'nativescript-vue';
+
+import router from './router';
+
+
+import './styles.scss';
+
+// Uncommment the following to see NativeScript-Vue output logs
+//Vue.config.silent = false;
+
+new Vue({
+
+  router,
+
+
+}).$start();
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'nativescript-vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+import Home from '../components/Home';
+import HelloWorld from '../components/HelloWorld';
+
+const router = new VueRouter({
+  pageRouting: true,
+  routes: [
+    {
+      path: '/home',
+      component: Home,
+      meta: {
+        title: 'Home',
+      },
+    },
+    {
+      path: '/hello',
+      component: HelloWorld,
+      meta: {
+        title: 'Hello World',
+      },
+    },
+    {path: '*', redirect: '/home'},
+  ],
+});
+
+router.replace('/home');
+
+module.exports = router;
