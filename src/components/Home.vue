62bd0da7438197cf24ace44777b7a3c8d445f3f9
Simon Cornet <simon@creative-atoms.be>
2018-03-06 08:39:06 +0100

    Get from URL

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,37 @@
 
     <StackLayout>
       <Label text="Hello world !" />
+      <Button text="Get from URL" @tap="axiosGet"/>
+
+      <Label textWrap="true">
+        <FormattedString>
+          <Span text="Data :" />
+          <Span :text="data" />
+        </FormattedString>
+      </Label>
     </StackLayout>
 
   </Page>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: ''
+    }
+  },
+  methods: {
+    axiosGet() {
+      this.axios.get('https://httpbin.org/get')
+        .then((response) => {
+          console.dir(response.data)
+          this.data = JSON.stringify(response.data)
+        }).catch((err)=>{
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
